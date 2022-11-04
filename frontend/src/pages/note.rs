7ed622ecb6f4d51b8083986411aea1a8730ad7f5
aichingert <aichinger.tobias13@gmail.com>
2022-11-04 22:38:58 +0100

    frontend (stable): changed note to be a component instead of a function

--- frontend/src/pages/note.rs
+++ frontend/src/pages/note.rs
@@ -1,16 +1,54 @@
 use yew::prelude::*;
 
+use crate::components::text_input::TextInput;
+
 #[derive(Properties, Clone, Debug, PartialEq, Eq)]
 pub struct Props;
 
-#[function_component]
-pub fn Note(_props: &Props) -> Html {
-    html! {
-        <>
-            <h1> { "Notes" } </h1>
 
-            <div>
-            </div>
-        </>
+pub struct Note {
+    log: String
+}
+
+pub enum Msg {
+    Write(String)
+}
+
+impl Note {
+    fn default() -> Self {
+        Self {
+            log: String::new()
+        }
     }
 }
+
+impl Component for Note {
+    type Message = Msg; 
+    type Properties = ();
+
+    fn create(_ctx: &yew::Context<Self>) -> Self {
+        Note::default()
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Write(value) => self.log = value
+        }
+
+        true
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+
+        html! {
+            <> 
+                <h1> { "Notes" } </h1>
+
+                <div>
+                </div>
+            </>
+        }   
+    }
+
+}
+
