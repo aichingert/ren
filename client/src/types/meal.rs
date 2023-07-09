4e37a8ffeafd247061e3d947e3bac738e4e09d22
aichingert <aichinger.tobias13@gmail.com>
2023-07-09 18:57:53 +0200

    feat: leptos here I come

--- client/src/types/meal.rs
+++
@@ -1,9 +0,0 @@
-pub struct Meal {
-    pub name: String,
-}
-
-impl Meal {
-    pub fn new(name: String) -> Self {
-        Self { name }
-    }
-}
--- server/src/db/model.rs
+++
@@ -1,7 +0,0 @@
-use sqlx::FromRow;
-
-#[derive(FromRow)]
-pub struct Recipe {
-    pub id: i64,
-    pub name: String,
-}
