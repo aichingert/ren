dbbc5570aee39aa1ab68a522a85d3b8bc56f64ab
aichingert <aichinger.tobias13@gmail.com>
2024-08-20 21:44:39 +0200

    init: setup project using build.zig

---
+++ build.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const test_step = b.step("test", "Run all the tests");
+
+    const lib = b.addStaticLibrary(.{
+        .name = "ren",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibCpp();
+    lib.addIncludePath(b.path("."));
+
+    lib.addCSourceFiles(.{
+        .root = b.path("src"),
+        .files = &.{"main.cpp"},
+        .flags = &.{},
+    });
+
+    const bindings = b.addModule("ren", .{
+        .root_source_file = b.path("ren.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    //bindings.linkLibary(lib);
+    _ = bindings;
+
+    const test_target = b.addTest(.{
+        .root_source_file = b.path("ren.zig"),
+    });
+    test_step.dependOn(&b.addRunArtifact(test_target).step);
+}
---
+++ example/src/main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try bw.flush(); // don't forget to flush!
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
