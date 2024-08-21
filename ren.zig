18eaa05ff468da9e2d86d9a0df10b8f225298581
aichingert <aichinger.tobias13@gmail.com>
2024-08-21 15:38:40 +0200

    feat(window): added title to window and extracted code to different files

--- ren.zig
+++ ren.zig
@@ -1,11 +1,18 @@
-extern fn create_window(width: c_int, height: c_int) void;
+extern fn init_window(width: c_int, height: c_int, title: [*c]u8) void;
 
-pub fn createWindow(width: i32, height: i32) void {
-    create_window(@intCast(width), @intCast(height));
+/// creates a window if the target is native otherwise
+/// it creates the index.html and index.js file for the
+/// application the width and height are currently
+/// ignored since the canvas will be fullscreen anyways
+/// and the browser can then be resized (can be changed if many people want that)
+pub fn initWindow(width: i32, height: i32, title: []const u8) void {
+    init_window(
+        @intCast(width),
+        @intCast(height),
+        @as([*c]u8, @constCast(@alignCast(title))),
+    );
 }
 
 test "can access extern fn" {
-    //const std = @import("std");
-    createWindow(10, 20);
-    //try std.testing.expectEqual(add(10, 20), 30);
+    initWindow(10, 20, "example");
 }
